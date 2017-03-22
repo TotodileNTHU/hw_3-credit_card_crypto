9ba28efee1f8f0a664025f76eb35c54109e933af
wtlin1228 <wtlin0711@gmail.com>
2017-03-23 05:35:25 +0800

    sk_cipher, crypto_spec, metaprograming done

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,17 +1,23 @@
 require 'rbnacl/libsodium'
+require 'base64'
 
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    Base64.encode64(RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes))
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    Base64.encode64(simple_box.encrypt(document))
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    simple_box.decrypt(Base64.decode64(encrypted_cc))
   end
 end
--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher.rb'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -9,49 +10,71 @@ describe 'Test card info encryption' do
     @key = 3
   end
 
-  describe 'Using Caesar cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Permutation cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
+  describe 'Using metaprogramming' do
+    [SubstitutionCipher::Caesar, SubstitutionCipher::Permutation, DoubleTranspositionCipher].each do |cipher|
+      describe 'Using ' + cipher.to_s do
+        it 'should encrypt card information' do
+          enc = cipher.encrypt(@cc, @key)
+          enc.wont_equal @cc.to_s
+          enc.wont_be_nil
+        end
+    
+        it 'should decrypt text' do
+          enc = cipher.encrypt(@cc, @key)
+          dec = cipher.decrypt(enc, @key)
+          dec.must_equal @cc.to_s
+        end
+      end 
     end
   end
 
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
+  # it 'should generate new Base64 keys'
+  # it 'should generate different keys each time'
+  # it 'should encrypt credit card objects'
+  # it 'should decrypt encrypted credit card objects'
+  # it 'should not replay the same encryption twice'
+  # it 'should not yield depth in encryption'
 
-  describe 'Using Double Transposition Cipher' do
-    it 'should encrypt card information' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+  describe 'Using Modern Symmetric Key Cipher' do
+    it 'should generate new Base64 keys' do
+      key = ModernSymmetricCipher.generate_new_key
+      key.wont_be_nil
+    end
+
+    it 'should generate different keys each time' do
+      key1 = ModernSymmetricCipher.generate_new_key
+      key2 = ModernSymmetricCipher.generate_new_key
+      key1.wont_equal key2
+    end
+    
+    it 'should encrypt credit card objects' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, key)
       enc.wont_equal @cc.to_s
       enc.wont_be_nil
     end
-
-    it 'should decrypt text' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
-      dec = DoubleTranspositionCipher.decrypt(enc, @key)
+    
+    it 'should decrypt encrypted credit card objects' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      dec = ModernSymmetricCipher.decrypt(enc, key)
       dec.must_equal @cc.to_s
     end
+    
+    it 'should not replay the same encryption twice' do
+      key  = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc1.wont_equal enc2
+    end
+    
+    it 'should not yield depth in encryption' do
+      key  = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc1.wont_equal enc2
+    end
   end
-
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+  
+  
 end
